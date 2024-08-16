37b52b91512dffde3bc7a721000b950193b6baa7
Akash <akashmadugundi@gmail.com>
2024-08-16 22:59:35 +0530

    Made splash screen with logo and animations

--- FindIT/app/(tabs)/explore.tsx
+++ FindIT/app/(tabs)/explore.tsx
@@ -10,7 +10,7 @@ const Explore = () => {
     >
       <View style={styles.innerContainer}>
         <Text style={styles.text}>
-          fbdbbd
+          sffsf
         </Text>
       </View>
     </ScrollView>
--- FindIT/tailwind.config.js
+++ FindIT/tailwind.config.js
@@ -1,9 +1,22 @@
 // tailwind.config.js
 
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FFFFFF',  // white
+        secondary: {
+          DEFAULT: "#3B5ED5",   // light blue
+          1000: "#1F316F",   // Dark navy blue
+        },
+        black: "#000000",   //black
+      },
+      fontFamily: {
+        header: ["AlmendraSC-Regular", "SpaceMono-Regular"],
+        tagline: ['AllertaStencil-Regular', "SpaceMono-Regular"],
+      },
+    },
   },
   plugins: [],
 }
